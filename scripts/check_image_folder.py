e62003c810b7af498eb04ba940214fbf05bf4989
tydus <tydus@tydus.org>
2020-06-19 18:19:27 +0000

    Add more checks to check_image_folder.py

--- scripts/check_image_folder.py
+++ scripts/check_image_folder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 from PIL import Image
 import os
@@ -28,6 +28,12 @@ for fn in os.listdir(base_path):
 
     try:
         im = Image.open(path)
+        size = im.size
+        if max(size) < 256:
+            raise Exception("Size: (%d, %d)" % size)
+        ar = 1. * max(size) / min(size)
+        if ar > 2.5:
+            raise Exception("Size: %s, AR: %.3f" % (size, ar))
         if im.mode not in ['RGB', 'RGBA']:
             raise Exception("Not RGB")
         s = np.asarray(im.convert('HSV'), dtype='float32')[..., 1]/255.
@@ -35,5 +41,5 @@ for fn in os.listdir(base_path):
             raise Exception("Sat %.3f" % np.mean(s))
 
     except Exception as e:
-        print path, e
+        print("%-50s%s" % (path, e))
         if do: os.rename(path, os.path.join(backup_path, fn))
